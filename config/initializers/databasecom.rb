78d4a3d791ec7d8ce051b3745cb3072725806f32
Michael Stewart <mickle00@gmail.com>
2011-12-08 06:13:09 -0800

    removing gem

--- config/initializers/databasecom.rb
+++
@@ -1,9 +0,0 @@
-require 'databasedotcom'
-
-puts 'DOES THIS THING WORK'
-client = Databasedotcom::Client.new :client_id => "3MVG9QDx8IX8nP5QZJiiE8AVOUfSOI2YW5gCSOyKj3rd_am4aeBA3dqTYpqRbPEzIG7virWlwMPHWWSKgsrRj", :client_secret => "1328915638464117235"
-
-client.authenticate :username => "developer@michaelpatrickstewart.com", :password => "playstation00018adfbynAba8aMpn37zgSVrA" 
-
-puts client 
-
